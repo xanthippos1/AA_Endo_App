0314d1d9da8930e5b02ce70bea51633ad4d74126
Xan Magnisalis <xandev67@gmail.com>
2026-04-03 03:17:10 -0400

    web build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3408524494" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1969427397" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
